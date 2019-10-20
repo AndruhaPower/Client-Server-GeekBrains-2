7b76d7c9f248ae05b6a6a008462881a327a18784
AndruhaPower <pazit@yandex.ru>
2019-10-20 19:49:43 +0300

    removed Firebase pod

--- L1_GBVk/L1_GBVk/AppDelegate.swift
+++ L1_GBVk/L1_GBVk/AppDelegate.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import RealmSwift
-import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         Realm.Configuration.defaultConfiguration = Realm.Configuration(deleteRealmIfMigrationNeeded: true)
-        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
